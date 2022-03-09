92b9e0154f91d6f9eb64dbc51bc1408bd33a2b42
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-09 19:00:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tired
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            /*
+             *  
+             *  Unity Junior Developer - lesson 01
+             * 
+             */
+
+            bool isPlayingMode = false;
+
+            byte inventoryCellSize = 255;
+
+            int currentPlayerHealth = 100;
+
+            short currentPlayerDeathsCount = 4;
+
+            string currentPlayerName = "Jojo Mojo";
+
+            float currentPlayerExperence = 345.25f;
+
+            char currentPlayerFractionSuffix = 'P';
+
+            double FPS = 32.0;
+
+            long doneActionsByPlayer = 23454119;
+
+            uint currentPlayerGold = 10435;
+
+        }
+    }
+}
