98a6b20de545321c0f758ef82f2fb05f58105a2a
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-09 21:20:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут?\nМое имя ");
+            string userName = Console.ReadLine();
+
+            Console.Write("Сколько вам лет?\nВаш возраст ");
+            string userAge = Console.ReadLine();
+
+            Console.Write("Где вы работаете?\nМесто работы ");
+            string userJob = Console.ReadLine();
+
+            Console.Write("Ваща зарплата?\nМоя зарплата ");
+            string userSalary = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут "+ userName + ", мне "+ userAge + ", я работаю на/в "+ userJob + ", и моя зарплата "+ userSalary);
+
+        }
+    }
+}
