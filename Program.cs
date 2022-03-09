611e133483206e27377f2e19ab65c7f89bee7a49
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-09 21:29:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сообщение, которое будет повторяться");
+            string message = Console.ReadLine();
+
+            Console.Write("Сколько раз повторять сообщение?\n(Введите значение)");
+            int repeatCount = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<repeatCount; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
