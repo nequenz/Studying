02e66ea4bfae32df8a36473c722314b337bd6a1c
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-09 20:58:38 +0300

    Update Lessons05.cs

--- Lessons05.cs
+++ Lessons05.cs
@@ -1,70 +1,34 @@
-﻿using System;
+using System;
 
 namespace Tired
 {
     class Program
     {
-
-        private static int TryToConvert(string value)
-        {
-
-            int Result = 0;
-
-            try
-            {
-                Result = Convert.ToInt32(value);
-            }
-            catch
-            {
-
-                Console.WriteLine("Вы ввели некорректное значение?\nК сожалению задание не может продолжено!:(");
-
-                //без throw
-
-                return 0;
-
-            }
-
-            return Result;
-        }
-
         static void Main(string[] args)
         {
+            const int minutesPerPerson = 10;
 
-            /*
-             *  
-             *  Unity Junior Developer - lesson 05
-             *  *Старушки это боль
-             * 
-             */
-
-            const int MinutesPerPerson = 10;
+            const int нour = 60;
 
             int oldersCountInQueue = 0;
 
             int commonTimeToWait = 0;
 
-  
-
-
             Console.Write("Введите количество старушек, которые будут в очереди:");
 
-            
-            oldersCountInQueue = TryToConvert( Console.ReadLine() );
-
+            oldersCountInQueue = Convert.ToInt32(Console.ReadLine());
 
             if (oldersCountInQueue == 0) return;
 
-            commonTimeToWait = (MinutesPerPerson * oldersCountInQueue);
+            commonTimeToWait = (minutesPerPerson * oldersCountInQueue);
 
-            int TimeToWaitInMinutes = commonTimeToWait % 60;
+            int TimeToWaitInMinutes = commonTimeToWait % нour;
 
-            int TimeToWaitInHours = commonTimeToWait / 60;
+            int TimeToWaitInHours = commonTimeToWait / нour;
 
             Console.WriteLine("Общее время ожидание составляет " + TimeToWaitInHours+" час(-а/-ов) и "+ TimeToWaitInMinutes+" минут(-ы)!");
 
             Console.ReadLine();
-
         }
     }
 }
