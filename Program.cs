fd9fd23a706f46a8ca3f0dac273aaf40b90437f9
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-09 21:39:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ExitWord = "exit";
+            string inputWord = "";
+
+            while(inputWord != ExitWord)
+            {
+                Console.WriteLine("Введите слово для выхода из цикла:");
+                inputWord = Console.ReadLine();
+            }
+
+            Console.WriteLine("Вы покинули цикл!");
+        }
+    }
+}
