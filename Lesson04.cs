0942e8e4942201b115ce264ff13506d9cb62b2fc
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-09 21:00:30 +0300

    Update Lesson04.cs

--- Lesson04.cs
+++ Lesson04.cs
@@ -1,67 +1,28 @@
-﻿using System;
+using System;
 
 namespace Tired
 {
     class Program
     {
-
-        private static int TryToConvert(string value)
-        {
-
-            int Result = 0;
-
-            try
-            {
-                Result = Convert.ToInt32(value);
-            }
-            catch
-            {
-
-                Console.WriteLine("Вот зачем вы ввели неправильне значение?\nК сожалению вы без остались без золота и без кристалов!:(");
-
-                //без throw
-
-                return 0;
-
-            }
-
-            return Result;
-        }
-
         static void Main(string[] args)
         {
-
-            /*
-             *  
-             *  Unity Junior Developer - lesson 04
-             * 
-             */
-
-
             int crystalPrice = new Random().Next(15, 50);
 
             int crystalsToBuy = 0;
-
 
             int currentGoldCount = 0;
 
             int currentCrystalsCount = 0;
 
-
-
             Console.WriteLine("У данного торговца цена кристалла составляет:" + crystalPrice);
 
             Console.Write("Введите начальное количество золота:");
 
-            currentGoldCount = TryToConvert( Console.ReadLine() );
-
-            if (currentGoldCount == 0) return;
+            currentGoldCount = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Принято\nВведите количество алмазов, которое вы хотите купить:");
 
-            crystalsToBuy = TryToConvert( Console.ReadLine() );
-
-            if (crystalsToBuy == 0) return;
+            crystalsToBuy = Convert.ToInt32(Console.ReadLine());
 
             int dealPrice = crystalsToBuy * crystalPrice;
 
@@ -73,8 +34,7 @@ namespace Tired
 
             currentCrystalsCount += crystalsToBuy;
 
-            Console.WriteLine("Вы прибрели " + crystalsToBuy + " алмазов!\nТекущее золото:"+ currentGoldCount);
-
+            Console.WriteLine("Вы прибрели " + crystalsToBuy + " алмазов!\nТекущее золото:" + currentGoldCount);
         }
     }
 }
