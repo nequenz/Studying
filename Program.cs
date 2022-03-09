74d8a8a1b475af7967613000867cc8db9ea0e470
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-09 19:25:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tired
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            /*
+             *  
+             *  Unity Junior Developer - lesson 03
+             * 
+             */
+
+
+            const int maxPictureCount = 52;
+
+            const int rowOutputPictureCount = 3;
+
+            int fullRowsCount = maxPictureCount / rowOutputPictureCount;
+
+            int pictureOverCount = maxPictureCount % rowOutputPictureCount;
+
+            Console.WriteLine("Максимальное количество заполненных рядов можно вывести "+ fullRowsCount+"\nВ последнем ряду на " + pictureOverCount+" картинку больше!");
+
+        }
+    }
+}
