d6b245917f71196569686ae9fe0ff68ef355ca6e
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-10 21:04:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxAccumulatedValue = 100;
+            const int AccumulatorValue = 7;
+            int accumulatedValue = 0;
+
+            for(int i=1;i<=(MaxAccumulatedValue/AccumulatorValue); i++)
+            {
+                accumulatedValue = AccumulatorValue * i;
+
+                Console.WriteLine("Вывод:" + accumulatedValue);
+            }
+
+            Console.WriteLine("Работа окончена");
+        }
+    }
+}
