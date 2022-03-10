de32b5dc712e9a41e3505ff52841f78765c8ed08
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-10 21:17:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jojin";
+            string surname = "Mojo";
+            string swap = "";
+
+            Console.WriteLine("Имя:"+ name+"\nФамилия:"+ surname);
+
+            swap = name;
+            name = surname;
+            surname = swap;
+
+            Console.WriteLine("Имя:" + name + "\nФамилия:" + surname);
+        }
+    }
+}
