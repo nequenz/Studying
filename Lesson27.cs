8cd6a9cfc9fe5c7f35f1e2e8711b26c84fc4dd9a
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-21 23:21:52 +0300

    Update Lesson27.cs

--- Lesson27.cs
+++ Lesson27.cs
@@ -7,10 +7,8 @@ namespace Tired
         static void Main(string[] args)
         {
             const int rowSize = 30;
-            const int rowMiddleSize = rowSize / 3;
             const int MinRandomBound = 10;
             const int MaxRandomBound = 99;
-            const int EmptyPlace = 0;
             int inputShift;
 
             int[] row = new int[rowSize];
@@ -19,16 +17,7 @@ namespace Tired
 
             for (int i = 0; i < rowSize; i++)
             {
-                bool isInMiddle = (i > rowMiddleSize && i < (rowMiddleSize * 2));
-
-                if (isInMiddle)
-                {
-                    row[i] = new Random().Next(MinRandomBound, MaxRandomBound);
-                }
-                else
-                {
-                    row[i] = EmptyPlace;
-                }
+                row[i] = new Random().Next(MinRandomBound, MaxRandomBound);
 
                 Console.Write(row[i] + " ");
             }
@@ -37,16 +26,18 @@ namespace Tired
 
             inputShift = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < rowSize; i++)
+            for (int i = 0; i < inputShift; i++)
             {
-                bool isInRow = ((i - inputShift) >= 0);
+                int savedValue = row[0];
 
-                if (isInRow)
+                for (int k = 1; k < rowSize; k++)
                 {
-                    row[i - inputShift] = row[i];
+                    int currentElement = row[k];
 
+                    row[k - 1] = currentElement;
                 }
-                    row[i] = EmptyPlace;
+
+                row[rowSize-1] = savedValue;
             }
 
             Console.WriteLine("Сдвинутый массив:");
