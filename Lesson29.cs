5e612e9c6b90a00d1da4928f00c311488005efea
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-22 22:28:26 +0300

    Update Lesson29.cs

--- Lesson29.cs
+++ Lesson29.cs
@@ -9,27 +9,17 @@ namespace Tired
             DrawBar(100, 30, 10, 3);
         }
 
-        private static void DrawBar(int maxValue, int currentValue, int posX = 0, int posY = 0)
+        private static void DrawBar(int maxValue, int currentValue, int positionX = 0, int positionY = 0)
         {
             const int BarSize = 10;
             int barCellsCount = maxValue / BarSize;
+            int drawnCellCount = currentValue / barCellsCount;
 
-            for (int i = 0; i < posY; i++)
-            {
-                Console.WriteLine();
-            }
-
-            for (int i = 0; i < posX; i++)
-            {
-                Console.Write(' ');
-            }
-
+            Console.SetCursorPosition(positionX, positionY);
             Console.Write('[');
 
             for (int i = 0; i < BarSize; i++)
             {
-                int drawnCellCount = currentValue / barCellsCount;
-
                 if (i < drawnCellCount)
                 {
                     Console.Write('#');
