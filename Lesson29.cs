2e08a36dd37e52c7a47af3ec2261af1e49095b00
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-22 22:05:08 +0300

    Create Lesson29.cs

---
+++ Lesson29.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawBar(100, 30, 10, 3);
+        }
+
+        private static void DrawBar(int maxValue, int currentValue, int posX, int posY)
+        {
+            const int BarSize = 10;
+            int barCellsCount = maxValue / BarSize;
+
+            for (int i = 0; i < posY; i++)
+            {
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < posX; i++)
+            {
+                Console.Write(' ');
+            }
+
+            Console.Write('[');
+
+            for (int i = 0; i < BarSize; i++)
+            {
+                int drawnCellCount = currentValue / barCellsCount;
+
+                if (i < drawnCellCount)
+                {
+                    Console.Write('#');
+                }
+                else
+                {
+                    Console.Write('_');
+                }
+            }
+
+            Console.Write(']');
+        }
+    }
+}
