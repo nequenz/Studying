d2a1c5f6299f55a77fab1b469d2ebcf9df95179e
Slava <70316676+nequenz@users.noreply.github.com>
2022-03-24 22:14:13 +0300

    Create Lesson30.cs

---
+++ Lesson30.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int convertedValue = 0;
+
+            ConvertToInt(out convertedValue);
+        }
+
+        private static void ConvertToInt(out int outValue)
+        {
+            Console.Write("Ввод:");
+
+            while( int.TryParse( Console.ReadLine(), out outValue) == false )
+            {
+                Console.Write("Значение не конвертировано, попробуйте еще раз\nВвод:");
+            }
+
+        }
+    }
+}
