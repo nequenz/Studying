652bf9aaaa46da3936d8245c3a1b8c7a5afd1ca7
Slava <70316676+nequenz@users.noreply.github.com>
2022-04-07 19:40:00 +0300

    Create Lesson35.cs

---
+++ Lesson35.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ExitWord = "exit";
+            const string SumWord = "sum";
+            string currentWord = "";
+            List<int> numberList = new List<int>();
+
+            Console.WriteLine("Введите число чтобы добавить его в список или " + ExitWord + " или " + SumWord + " для выполнения команд");
+
+            while (currentWord != ExitWord)
+            {
+                Console.Write("Значение:");
+                currentWord = Console.ReadLine();
+
+                if( currentWord == ExitWord)
+                {
+                    continue;
+                }
+                else if(currentWord == SumWord)
+                {
+                    Console.WriteLine("Сумма всего списка равна:" + SumList(numberList));
+                }
+                else
+                {
+                    bool IsParsed = Int32.TryParse(currentWord, out int parsedValue);
+
+                    if(IsParsed == true)
+                    {
+                        numberList.Add(parsedValue);
+                        Console.WriteLine("Значение добавлено!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не удалось добавить значение!");
+                    }
+                }
+            }
+        }
+
+        private static int SumList(List<int> selectedList)
+        {
+            int result = 0;
+
+            foreach(int number in selectedList)
+            {
+                result += number;
+            }
+
+            return result;
+        }
+    }
+}
