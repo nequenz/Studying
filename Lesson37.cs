efd3bb516abf52d3aa3310afc719ddb6d8724d53
Slava <70316676+nequenz@users.noreply.github.com>
2022-04-11 20:33:22 +0300

    Update Lesson37.cs

--- Lesson37.cs
+++ Lesson37.cs
@@ -7,32 +7,30 @@ namespace Tired
     {
         static void Main(string[] args)
         {
-            string[] line0 = new string[] { "pool", "cool", "null", "crul" };
+            string[] line0 = new string[] { "pool", "cool", "null", "crul", "crul" };
             string[] line1 = new string[] { "dool", "null", "prool", "fool" };
+            List<string> list = new List<string>();
+
+            ConcateStringsToList(ref list, line0);
+            ConcateStringsToList(ref list, line1);
 
             Console.WriteLine("Результат:");
 
-            foreach(string element in ConcateStrings(line0, line1))
+            foreach(string element in list)
             {
                 Console.Write(element+" ");
             }
         }
 
-        private static List<string> ConcateStrings(string[] line0, string[] line1)
+        private static void ConcateStringsToList(ref List<string> outList,string[] line)
         {
-            List<string> resultList = new List<string>();
-
-            resultList.AddRange(line0);
-
-            foreach(string stringElement in line1)
+            foreach(string element in line)
             {
-                if(resultList.Contains(stringElement) == false)
+                if(outList.Contains(element) == false)
                 {
-                    resultList.Add(stringElement);
+                    outList.Add(element);
                 }
             }
-
-            return resultList;
         }
     }
 }
