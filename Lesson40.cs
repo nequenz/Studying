087d6217fb96e8c458b78f81851e7e74eb368698
Slava <70316676+nequenz@users.noreply.github.com>
2022-04-14 19:56:50 +0300

    Update Lesson40.cs

--- Lesson40.cs
+++ Lesson40.cs
@@ -8,19 +8,106 @@ namespace Tired
         static void Main(string[] args)
         {
             PlayerCollector playerCollector = new PlayerCollector();
+            PlayerCollectorMenu menu = new PlayerCollectorMenu(playerCollector);
 
-            playerCollector.Add(new Player("Lolz",4));
-            playerCollector.Add(new Player("DarkStalker98", 1));
-            playerCollector.Add(new Player("PinkPopa", 45));
-            playerCollector.Add(new Player("superAnton2004", 12));
+            menu.UpdateInput();
+        }
+    }
 
-            playerCollector.SetPlayerBanMode(3, true);
-            playerCollector.ShowEntities();
+    class PlayerCollectorMenu
+    {
+        private const string WordToExit = "exit";
+        private const string WordToAddPlayer = "add";
+        private const string WordToRemovePlayer = "remove";
+        private const string WordToBanPlayer = "ban";
+        private const string WordToUnbanPlayer = "unban";
 
-            Console.WriteLine("-----------------------");
+        private string _wordToInput = "";
+        private PlayerCollector _collector;
 
-            playerCollector.RemoveByID(2);
-            playerCollector.ShowEntities();
+        public PlayerCollectorMenu(PlayerCollector collector)
+        {
+            _collector = collector;
+        }
+
+        public void UpdateInput()
+        {
+            Console.WriteLine("Введите команды для выполнения действия с базой данных");
+
+            while(_collector!=null && _wordToInput != WordToExit)
+            {
+                Console.Write("Введите команду:");
+                _wordToInput = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (_wordToInput)
+                {
+                    case WordToExit:
+                        break;
+                    case WordToAddPlayer:
+                        AddPlayerByUserInput();
+                        break;
+                    case WordToRemovePlayer:
+                        RemovePlayerByUserInput();
+                        break;
+                    case WordToBanPlayer:
+                        SetPlayerBanModeByUserInput(true);
+                        break;
+                    case WordToUnbanPlayer:
+                        SetPlayerBanModeByUserInput(false);
+                        break;
+                }
+            }
+        }
+
+        private bool TryReadPlayerIdByUserInput(out int id)
+        {
+            Console.Write("Введите ID:");
+
+            return Int32.TryParse( Console.ReadLine(), out id);
+        }
+
+        private void AddPlayerByUserInput()
+        {
+            string nickname;
+            int level;
+
+            Console.Write("Введите ник игрока:");
+            nickname = Console.ReadLine();
+
+            Console.Write("Введите уровень игрока:");
+
+            if(Int32.TryParse( Console.ReadLine(),out level))
+            {
+                _collector.Add(new Player(nickname,level));
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Игрок не был добавлен...");
+            }
+        }
+
+        private void RemovePlayerByUserInput()
+        {
+            if( TryReadPlayerIdByUserInput(out int id) && (_collector.RemoveByID(id) == true) )
+            {
+                Console.WriteLine("Игрок под id " + id + " был удален!");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Игрок не был удален...");
+            }
+
+        }
+
+        private void SetPlayerBanModeByUserInput(bool isBanned)
+        {
+            if ( TryReadPlayerIdByUserInput(out int id) )
+            {
+                _collector.SetPlayerBanMode(id, isBanned);
+
+                Console.WriteLine("Режим бана игрока под id " + id + ": " + isBanned.ToString());
+            }
         }
     }
 
@@ -38,22 +125,22 @@ namespace Tired
 
         public void SetBanMode(bool isBanned) => IsBanned = isBanned;
     }
-        
+
     sealed class PlayerCollector : EntityCollector
     {
         public Player GetPlayerByID(int id) => GetEntityByID(id) as Player;
 
-        public void SetPlayerBanMode(int id,bool isBanned) => GetPlayerByID(id)?.SetBanMode(isBanned);
+        public void SetPlayerBanMode(int id, bool isBanned) => GetPlayerByID(id)?.SetBanMode(isBanned);
     }
 
     class EntityCollector : ICollectorable
     {
         protected List<CollectedEntity> _entityList = new List<CollectedEntity>();
         private int _nextID = 0;
-        
+
         public bool Add(CollectedEntity entity)
         {
-            if(entity !=null && entity.ID == ICollectorable.EmptyID)
+            if (entity != null && entity.ID == ICollectorable.EmptyID)
             {
                 entity.ID = _nextID;
                 _nextID++;
@@ -81,9 +168,9 @@ namespace Tired
 
         public CollectedEntity GetEntityByID(int id)
         {
-            foreach(CollectedEntity entity in _entityList)
+            foreach (CollectedEntity entity in _entityList)
             {
-                if(entity.ID == id)
+                if (entity.ID == id)
                 {
                     return entity;
                 }
@@ -96,12 +183,12 @@ namespace Tired
         {
             foreach (CollectedEntity entity in _entityList)
             {
-                Console.WriteLine("Объект типа "+entity.ToString()+" с ид "+entity.ID);
+                Console.WriteLine("Объект типа " + entity.ToString() + " с ид " + entity.ID);
             }
         }
     }
 
-    abstract class CollectedEntity 
+    abstract class CollectedEntity
     {
         private int _id = ICollectorable.EmptyID;
 
