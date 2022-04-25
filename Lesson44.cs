9c58df81d323eef2aa293c39f758a800af1e9b8d
Slava <70316676+nequenz@users.noreply.github.com>
2022-04-25 20:50:12 +0300

    Update Lesson44.cs

--- Lesson44.cs
+++ Lesson44.cs
@@ -7,13 +7,12 @@ namespace Tired
     {
         static void Main(string[] args)
         {
-          
         }
     }
 
     class BigСarriage : Сarriage
     {
-      
+
         public BigСarriage() : base(96)
         {
 
@@ -22,6 +21,7 @@ namespace Tired
 
     abstract class Сarriage
     {
+        private const int BadIndex = -1;
         public PassengerPlace[] PassengerPlaces { get; private set; }
 
         public Сarriage(int placeCount)
@@ -31,7 +31,7 @@ namespace Tired
 
         public void ClearPassengerPlaces()
         {
-            for(int i = 0; i < PassengerPlaces.Length; i++)
+            for (int i = 0; i < PassengerPlaces.Length; i++)
             {
                 PassengerPlaces[i].Clear();
             }
@@ -45,7 +45,7 @@ namespace Tired
 
             for (int i = 0; i < PassengerPlaces.Length; i++)
             {
-                if( PassengerPlaces[i].IsFree() == true)
+                if (PassengerPlaces[i].IsFree() == true)
                 {
                     count++;
                 }
@@ -54,22 +54,78 @@ namespace Tired
             return count;
         }
 
-        public int GetOccupiedPlacesCount() => ( GetPlaceCount() - GetFreePlaceCount() );
+        public int GetOccupiedPlacesCount() => (GetPlaceCount() - GetFreePlaceCount());
 
-        public bool TryAddPassenger()
+        public bool TryAddPassenger(PassengerPlace place)
         {
+            int index = FindFirstFreePlaceIndex();
 
+            if(index != BadIndex)
+            {
+                PassengerPlaces[index] = place;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryRemovePassenger(PassengerPlace place)
+        {
+            int index = FindIndexByPassengerPlace(place);
+
+            if(index != BadIndex)
+            {
+                PassengerPlaces[index].Clear();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private int FindIndexByPassengerPlace(PassengerPlace place)
+        {
+            for (int i = 0; i < PassengerPlaces.Length; i++)
+            {
+                if (PassengerPlaces[i].Equals(place) == true)
+                {
+                    return i;
+                }
+            }
+
+            return BadIndex;
+        }
+    
+        private int FindFirstFreePlaceIndex()
+        {
+            for (int i = 0; i < PassengerPlaces.Length; i++)
+            {
+                if (PassengerPlaces[i].IsFree() == true)
+                {
+                    return i;
+                }
+            }
+
+            return BadIndex;
         }
     }
 
     struct PassengerPlace
     {
         public string FullName { get; private set; }
+        public string Passport { get; private set; }
 
         public void SetFullName(string name) => FullName = name;
 
-        public void Clear() => FullName = "";
+        public void SetPassport(string passport) => Passport = passport;
 
-        public bool IsFree() => (FullName == "");
+        public void Clear()
+        {
+            FullName = "";
+            Passport = "";
+        }
+
+        public bool IsFree() => (FullName == "" && Passport == "");
+
     }
 }
