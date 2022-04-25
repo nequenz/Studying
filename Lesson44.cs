18df44587cf125c206d2165f693f0cc0f01f101b
Slava <70316676+nequenz@users.noreply.github.com>
2022-04-25 19:25:54 +0300

    Create Lesson44.cs

---
+++ Lesson44.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+
+    class BigTrain : Train
+    {
+
+    }
+
+    class Train
+    {
+        public PassengerPlace[] PassengerPlaces { get; private set; }
+    }
+
+    class PassengerPlace
+    {
+
+    }
+}
