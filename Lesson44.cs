4ddecd132a703f761b002d72ff48f0329689a179
Slava <70316676+nequenz@users.noreply.github.com>
2022-04-25 19:58:49 +0300

    Update Lesson44.cs

--- Lesson44.cs
+++ Lesson44.cs
@@ -11,18 +11,65 @@ namespace Tired
         }
     }
 
-    class BigTrain : Train
+    class BigСarriage : Сarriage
     {
+      
+        public BigСarriage() : base(96)
+        {
 
+        }
     }
 
-    class Train
+    abstract class Сarriage
     {
         public PassengerPlace[] PassengerPlaces { get; private set; }
+
+        public Сarriage(int placeCount)
+        {
+            PassengerPlaces = new PassengerPlace[placeCount];
+        }
+
+        public void ClearPassengerPlaces()
+        {
+            for(int i = 0; i < PassengerPlaces.Length; i++)
+            {
+                PassengerPlaces[i].Clear();
+            }
+        }
+
+        public int GetPlaceCount() => PassengerPlaces.Length;
+
+        public int GetFreePlaceCount()
+        {
+            int count = 0;
+
+            for (int i = 0; i < PassengerPlaces.Length; i++)
+            {
+                if( PassengerPlaces[i].IsFree() == true)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int GetOccupiedPlacesCount() => ( GetPlaceCount() - GetFreePlaceCount() );
+
+        public bool TryAddPassenger()
+        {
+
+        }
     }
 
-    class PassengerPlace
+    struct PassengerPlace
     {
+        public string FullName { get; private set; }
 
+        public void SetFullName(string name) => FullName = name;
+
+        public void Clear() => FullName = "";
+
+        public bool IsFree() => (FullName == "");
     }
 }
