64e9539e118b2c1db8cf5a87d934b9c07701afaf
Slava <70316676+nequenz@users.noreply.github.com>
2022-06-11 01:52:11 +0300

    Create Lesson58.cs

---
+++ Lesson58.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tired
+{
+    public class Program
+    {
+        private static List<Fighter> _teamA = new List<Fighter>();
+        private static List<Fighter> _teamB = new List<Fighter>();
+
+        private static void Main(string[] args)
+        {
+            const char startChar = 'Б';
+
+            CreateTeam(_teamA);
+            CreateTeam(_teamB);
+
+            Console.WriteLine("--Команда А--");
+            PrintTeam(_teamA);
+
+            Console.WriteLine("--Команда Б--");
+            PrintTeam(_teamB);
+
+            Console.WriteLine("--Команда Б с командой А--");
+
+            var team = from Fighter fighter in _teamA
+                       where fighter.Name.StartsWith(startChar)
+                       select fighter;
+
+            var unionTeam = _teamB.Union(team);
+
+            PrintTeam(unionTeam);
+        }
+
+        public static void CreateTeam(List<Fighter> list)
+        {
+            const int maxCount = 25;
+            int randomCount = StudyHelper.GetRandomValue(0, maxCount);
+
+            for (int i = 0; i < randomCount; i++)
+            {
+                list.Add(new Fighter(StudyHelper.GetRandomName()));
+            }
+        }
+
+        public static void PrintTeam(IEnumerable<Fighter> list)
+        {
+            foreach (Fighter fighter in list)
+            {
+                Console.WriteLine(fighter.Name);
+            }
+        }
+    }
+
+    public class Fighter
+    {
+        public string Name { get; private set; } = "Безымянный боец";
+
+        public Fighter(string name) => Name = name;
+    }
+
+    public static class StudyHelper
+    {
+        public static int GetRandomValue(int minValue = 0, int maxValue = 1) => new Random().Next(minValue, maxValue);
+
+        public static string GetRandomName()
+        {
+            const int maxChars = 5;
+
+            string name = "";
+
+            for (int i = 0; i < maxChars; i++)
+            {
+                name += GetRandomRussianChar();
+            }
+
+            return name;
+        }
+
+        public static char GetRandomRussianChar()
+        {
+            const int codePageStart = 1040;
+            const int codePageEnd = 1071;
+
+            return (char)GetRandomValue(codePageStart, codePageEnd + 1);
+        }
+    }
+}
