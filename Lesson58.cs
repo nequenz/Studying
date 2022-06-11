1e9291ea9b79a5f733b12c6616f44e3e066769a9
Slava <70316676+nequenz@users.noreply.github.com>
2022-06-11 22:19:45 +0300

    Update Lesson58.cs
    Signed-off-by: Slava <70316676+nequenz@users.noreply.github.com>

--- Lesson58.cs
+++ Lesson58.cs
@@ -6,15 +6,26 @@ namespace Tired
 {
     public class Program
     {
+        private static void Main(string[] args)
+        {
+            FightClub club = new FightClub();
+
+            club.UnionTeams();
+        }
+    }
+
+    public class FightClub
+    {
         private static List<Fighter> _teamA = new List<Fighter>();
         private static List<Fighter> _teamB = new List<Fighter>();
 
-        private static void Main(string[] args)
+        public void UnionTeams()
         {
             const char StartChar = 'Б';
+            Func<Fighter, bool> conditionToSelect = (fighter => fighter.Name.StartsWith(StartChar) == true);
 
-            CreateTeam(_teamA);
-            CreateTeam(_teamB);
+            OrganizeTeam(_teamA);
+            OrganizeTeam(_teamB);
 
             Console.WriteLine("--Команда А--");
             PrintTeam(_teamA);
@@ -24,27 +35,30 @@ namespace Tired
 
             Console.WriteLine("--Команда Б с командой А--");
 
-            var team = from Fighter fighter in _teamA
-                       where fighter.Name.StartsWith(StartChar)
-                       select fighter;
+            _teamB.AddRange(_teamA.Where(conditionToSelect));
+            _teamA.RemoveAll(new Predicate<Fighter>(conditionToSelect));
 
-            var unionTeam = _teamB.Union(team);
+            PrintTeam(_teamB);
 
-            PrintTeam(unionTeam);
+            Console.WriteLine("--Остатки команды А--");
+            PrintTeam(_teamA);
         }
 
-        public static void CreateTeam(List<Fighter> list)
+        private void OrganizeTeam(List<Fighter> list)
         {
             const int MaxCount = 25;
-            int randomCount = StudyHelper.GetRandomValue(0, MaxCount);
+
+            StudyRandomizer randomizer = new StudyRandomizer();
+            CodePages codePages = new CodePages();
+            int randomCount = randomizer.GetRandomValue(0, MaxCount);
 
             for (int i = 0; i < randomCount; i++)
             {
-                list.Add(new Fighter(StudyHelper.GetRandomName()));
+                list.Add(new Fighter(randomizer.GetRandomName(5, codePages.GetCodePage("RussianTest"))));
             }
         }
 
-        public static void PrintTeam(IEnumerable<Fighter> list)
+        private void PrintTeam(IEnumerable<Fighter> list)
         {
             foreach (Fighter fighter in list)
             {
@@ -55,35 +69,76 @@ namespace Tired
 
     public class Fighter
     {
-        public string Name { get; private set; } = "Безымянный боец";
+        public string Name { get; private set; }
 
         public Fighter(string name) => Name = name;
     }
 
-    public static class StudyHelper
+    public struct CodePage
     {
-        public static int GetRandomValue(int minValue = 0, int maxValue = 1) => new Random().Next(minValue, maxValue);
+        private int _pageStart;
+        private int _pageEnd;
+        private string _name;
 
-        public static string GetRandomName()
+        public int PageStart
         {
-            const int MaxChars = 5;
+            get => _pageStart;
+        }
 
+        public int PageEnd
+        {
+            get => _pageEnd;
+        }
+
+        public string Name
+        {
+            get => _name;
+        }
+
+        public CodePage(string name, int start, int end)
+        {
+            _name = name;
+            _pageStart = start;
+            _pageEnd = end;
+        }
+    }
+
+    public class CodePages
+    {
+        private List<CodePage> _pages = new List<CodePage>();
+
+        public CodePages()
+        {
+            InitDefaultPages();
+        }
+
+        private void InitDefaultPages()
+        {
+            _pages.Add(new CodePage("RussianUpper", 1040, 1071));
+            _pages.Add(new CodePage("RussianTest", 1040, 1044));
+        }
+
+        public CodePage GetCodePage(string name) => _pages.Find(codePage => codePage.Name == name);
+    }
+
+    public class StudyRandomizer
+    {
+        private Random _randomizer = new Random();
+
+        public int GetRandomValue(int minValue = 0, int maxValue = 1) => _randomizer.Next(minValue, maxValue + 1);
+
+        public string GetRandomName(int charCount, CodePage page)
+        {
             string name = "";
 
-            for (int i = 0; i < MaxChars; i++)
+            for (int i = 0; i < charCount; i++)
             {
-                name += GetRandomRussianChar();
+                name += GetRandomChar(page);
             }
 
             return name;
         }
 
-        public static char GetRandomRussianChar()
-        {
-            const int CodePageStart = 1040;
-            const int CodePageEnd = 1071;
-
-            return (char)GetRandomValue(CodePageStart, CodePageEnd + 1);
-        }
+        public char GetRandomChar(CodePage page) => (char)GetRandomValue(page.PageStart, page.PageEnd);
     }
 }
